638c6ab01ef0a5c1e95aad8402b14a8ba945a6ab
aline-goncalves <toneti.aline@gmail.com>
2023-03-28 16:21:05 -0300

    implementing URLs validation

--- src/cli.js
+++ src/cli.js
@@ -1,18 +1,31 @@
 import chalk from 'chalk';
 import returnLinksFromPath from './index.js';
+import validateList from './http_validation.js';
 
 const path = process.argv;
 
-async function textProcess(argumentPath){
-    const pathForVerification = argumentPath[2];
+function interpretCommands(argument){
+    const pathForVerification = argument[2];
+    const validate = argument[3] === '--validate';
+
+    textProcess(validate, pathForVerification);
+}
+
+async function textProcess(validate, pathForVerification){    
     const result = await returnLinksFromPath(pathForVerification);
-    printList(result);
+    await printList(validate, result);
 }
 
-function printList(linkList){
+async function printList(validate, linkList){
     const messageNotFound = 'Links not found!';
+    if(validate){
+        const validedLinks = JSON.stringify(await validateList(linkList));
+        console.log(chalk.cyan('status dos links validados: '), 
+                chalk.magenta(typeof(validedLinks) === 'undefined' ? messageNotFound : validedLinks));
+                return;
+    }
     console.log(chalk.cyan('lista de links: '), 
-                chalk.magenta(linkList == 'undefined' ? linkList : messageNotFound));
+                chalk.magenta(typeof(linkList) === 'undefined' ? messageNotFound : JSON.stringify(linkList)));
 }
 
-textProcess(path);
+interpretCommands(path);
